446d11db18a2252b2d82fcf798f15efbae8732c7
gerardo <up240497@alumnos.upa.edu.mxo>
2025-11-29 10:48:31 -0600

    Primer commit

---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+	public static void (String[] args){
+		System.out.println("hola mundooo");
+		System.out.println("---archivo original ----");
+		System.out.println("nueva linea y primer commit");
+	}
+}
